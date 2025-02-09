08349726a16f4f9a0f7f2825a13acbcb6281db92
Pedro Badaro <badaro1988@gmail.com>
2025-02-09 18:39:21 +0800

    Corrige layout do carrossel e melhora responsividade

--- script.js
+++ script.js
@@ -14,7 +14,17 @@ $(document).ready(function() {
         infinite: true,
         speed: 500,
         fade: true,
-        cssEase: 'linear'
+        cssEase: 'linear',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
     });
 
     // Configuração das máscaras
